6efdfb5686244e2cd1acfa78bbd69b976bf20807
rmarki <88067951+rmarki@users.noreply.github.com>
2021-08-04 08:57:14 -0600

    Update setup_cch.cpp

--- Explicit-Lagrange/src/CCH_solver/setup_cch.cpp
+++ Explicit-Lagrange/src/CCH_solver/setup_cch.cpp
@@ -11,7 +11,7 @@
 #include "variables.h"
 
 using namespace utils;
-#define PI 3.14159265
+#define PI (3.1415926535897932384626433832795)
 
 // -----------------------------------------------------------------------------
 // This function reads the mesh and sets up the simulation
