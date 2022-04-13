7b5cb32e4c86798ca8a309869114926bb538e896
Nathaniel Ray Morgan - 178925 <nmorgan@darwin-fe1.lanl.gov>
2022-04-13 12:35:41 -0600

    added cuda and hip build scripts for the 1D SGH code

---
+++ scripts/1D-SGH/kokkos-cuda-amp/sourceme-env.sh
@@ -0,0 +1,28 @@
+module purge
+### Load environment modules here
+module load cmake/3.18.1
+module load gcc/9.4.0
+module load cuda/11.4.0
+module list
+
+export scriptdir=`pwd`
+
+cd ../../..
+export basedir=`pwd`
+
+export matardir=${basedir}/Elements/matar
+
+export srcdir=${basedir}/Explicit-Lagrange-Kokkos/1D_SGH_solver
+export builddir=${basedir}/build-1DSGH-cuda-amp
+export installdir=${basedir}/build-kokkos/install-kokkos-cuda-amp
+
+export SGH_BASE_DIR=${basedir}
+export SGH_SOURCE_DIR=${srcdir}
+export SGH_BUILD_DIR=${builddir}
+
+export KOKKOS_SOURCE_DIR=${matardir}/src/Kokkos/kokkos
+export KOKKOS_BUILD_DIR=${builddir}/kokkos
+export KOKKOS_INSTALL_DIR=${installdir}/kokkos
+
+cd $scriptdir
+
