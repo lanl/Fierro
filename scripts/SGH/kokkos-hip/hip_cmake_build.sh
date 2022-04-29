0e2327f38d6ffa4bb8db1412d52b8547f6aaceec
Nathaniel Ray Morgan - 178925 <nmorgan@darwin-fe1.lanl.gov>
2022-04-29 14:21:48 -0600

    added hip build scripts for SGH with Kokkos

---
+++ scripts/SGH/kokkos-hip/hip_cmake_build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+rm -rf ${SGH_BUILD_DIR}
+mkdir -p ${SGH_BUILD_DIR}
+cd ${SGH_BUILD_DIR}
+
+OPTIONS=(
+-D BUILD_KOKKOS_SGH=ON
+-D BUILD_ELEMENTS=OFF
+-D BUILD_EXPLICIT_SOLVER=OFF
+-D BUILD_IMPLICIT_SOLVER=OFF
+#-D BUILD_SGH=ON
+-D KOKKOS=ON
+-D HIP=ON
+-D CMAKE_CXX_COMPILER=hipcc
+-D Kokkos_DIR=${KOKKOS_INSTALL_DIR}/lib64/cmake/Kokkos
+)
+set -x
+cmake "${OPTIONS[@]}" "${SGH_BASE_DIR:-../}"
+set +x
+make -j16 -l32
+
+cd $basedir
