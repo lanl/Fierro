23101dc396dc89ad76b1aca08b561f89190b2ff1
Daniel Dunning <ddunning@lanl.gov>
2023-08-29 15:50:19 -0600

    almost done with single-node scripts.

--- multi-node/scripts/1D-SGH/kokkos-cuda/build-it.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-source sourceme-env.sh
-source kokkos-install.sh
-source cuda_cmake_build.sh
--- multi-node/scripts/SGH/kokkos-macOS/build-it.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-source sourceme-env.sh
-source kokkos-install.sh
-source serial_cmake_build.sh
