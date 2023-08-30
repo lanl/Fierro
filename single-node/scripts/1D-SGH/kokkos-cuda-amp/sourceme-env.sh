aa6faef92d306e2e5faac6fa3ec82c69bc81637c
Daniel Dunning <ddunning@lanl.gov>
2023-08-30 11:39:41 -0600

    copied SGH scripts over to 1D. Currently testing functionality

---
+++ single-node/scripts/1D-SGH/kokkos-cuda-amp/sourceme-env.sh
@@ -0,0 +1,28 @@
+module purge
+### Load environment modules here
+module load cmake
+module load gcc/9.4.0
+module load cuda/11.4.0
+module list
+
+export scriptdir=`pwd`
+
+cd ../../../..
+export topdir=`pwd`
+export basedir=${topdir}/single-node
+export srcdir=${basedir}/src
+export includedir=${basedir}/include
+export matardir=${includedir}/matar
+export builddir=${basedir}/build-1DSGH-cuda-amp
+export installdir=${basedir}/install-kokkos/install-kokkos-cuda-amp
+
+export SGH_BASE_DIR=${basedir}
+export SGH_SOURCE_DIR=${srcdir}/Explicit-Lagrange-Kokkos/1D_SGH_solver
+export SGH_BUILD_DIR=${builddir}
+
+export KOKKOS_SOURCE_DIR=${matardir}/src/Kokkos/kokkos
+export KOKKOS_BUILD_DIR=${builddir}/kokkos
+export KOKKOS_INSTALL_DIR=${installdir}/kokkos
+
+cd $scriptdir
+
---
+++ single-node/scripts/SGH/kokkos-openmp/openmp_cmake_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+rm -rf ${SGH_BUILD_DIR}
+mkdir -p ${SGH_BUILD_DIR}
+cd ${SGH_BUILD_DIR}
+
+OPTIONS=(
+-D BUILD_KOKKOS_SGH=ON
+-D BUILD_EXPLICIT_SOLVER=OFF
+-D KOKKOS=ON
+-D OPENMP=ON
+-D Kokkos_DIR=${KOKKOS_INSTALL_DIR}/lib64/cmake/Kokkos
+)
+set -x
+cmake "${OPTIONS[@]}" "${SGH_BASE_DIR:-../}"
+set +x
+make -j16 -l32
+
+cd $basedir
