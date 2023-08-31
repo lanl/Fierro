5d8f956a3ee3ef0067c9fa7d0658bd67fd12b228
Daniel Dunning <ddunning@lanl.gov>
2023-08-31 15:03:11 -0600

    scripts trimmed down and mostly tested

---
+++ single-node/scripts/SGH/kokkos/build-it.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+SYSTEM=$1
+PARALLEL=$2
+CUSTOM_BUILD=$3
+
+source sourceme-env.sh ${SYSTEM} ${PARALLEL} ${CUSTOM_BUILD}
+source kokkos-install.sh ${SYSTEM} ${PARALLEL}
+source cmake_build.sh ${SYSTEM} ${PARALLEL}
