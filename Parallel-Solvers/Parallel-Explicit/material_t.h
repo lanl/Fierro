c1f94ab96dfb3895af982bbfe8f7452956e7a8c1
Kevin Welsh <kwelsh@lanl.gov>
2023-09-08 09:59:09 -0600

    Remove noop code

--- Parallel-Solvers/Parallel-Explicit/material_t.h
+++ Parallel-Solvers/Parallel-Explicit/material_t.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "matar.h"
- #include "yaml-serializable.h"
+#include "yaml-serializable.h"
 
 using namespace mtr;
 
@@ -23,30 +23,12 @@ struct material_t {
   double q2ex;
   
   size_t num_global_vars = 0;
-      
-  //eos_function_type* eos_model = NULL;
-  //strength_function_type* strength_model = NULL;
-  
-  KOKKOS_FUNCTION
-  void derive_function_pointers_no_exec() {
-  }
-
-  void derive_function_pointers() {
-    derive_function_pointers_no_exec();
-    if (eos_model == EOS_MODEL::none) {
-      throw Yaml::ConfigurationException("Unsupported EOS MODEL type: " + to_string(eos_model));
-    }
-  }
 };
 
-struct Material : Yaml::DerivedFields, material_t {
+struct Material : material_t {
   std::vector<double> global_vars;
-
-  void derive() {
-    num_global_vars = global_vars.size();
-    derive_function_pointers();
-  }
 };
+
 IMPL_YAML_SERIALIZABLE_FOR(Material, 
   eos_model, strength_model, strength_type,
   strength_run_location, eos_run_location,
