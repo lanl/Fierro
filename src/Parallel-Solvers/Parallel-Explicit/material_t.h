870c20ce845058a770dd827ebb1aa5e6913340f8
Kevin Welsh <kwelsh@lanl.gov>
2023-09-21 11:08:37 -0600

    remove none as an EOS_MODEL option

--- src/Parallel-Solvers/Parallel-Explicit/material_t.h
+++ src/Parallel-Solvers/Parallel-Explicit/material_t.h
@@ -5,13 +5,13 @@
 
 using namespace mtr;
 
-SERIALIZABLE_ENUM(EOS_MODEL, none, ideal_gas, user_eos_model)
+SERIALIZABLE_ENUM(EOS_MODEL, ideal_gas, user_eos_model)
 SERIALIZABLE_ENUM(STRENGTH_MODEL, none, ideal_gas, user_strength_model)
 SERIALIZABLE_ENUM(STRENGTH_TYPE, none, hypo, hyper)
 SERIALIZABLE_ENUM(RUN_LOCATION, device, host)
 
 struct material_t {
-  EOS_MODEL eos_model                = EOS_MODEL::none;
+  EOS_MODEL eos_model                = EOS_MODEL::ideal_gas;
   STRENGTH_MODEL strength_model      = STRENGTH_MODEL::none;
   STRENGTH_TYPE strength_type        = STRENGTH_TYPE::none;
   RUN_LOCATION strength_run_location = RUN_LOCATION::device;
@@ -28,7 +28,7 @@ struct material_t {
 struct Material : material_t {
   std::vector<double> global_vars;
 };
-
+YAML_ADD_REQUIRED_FIELDS_FOR(Material, eos_model)
 IMPL_YAML_SERIALIZABLE_FOR(Material, 
   eos_model, strength_model, strength_type,
   strength_run_location, eos_run_location,
