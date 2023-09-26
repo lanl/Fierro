843cda70df2f237adc733dfde7a9c5c4fbcb8df1
Kevin Welsh <kwelsh@lanl.gov>
2023-09-26 11:05:53 -0600

    create anaconda package/build metadata for EVPFFT

---
+++ Anaconda-Packages/evpfft/cuda/build.sh
@@ -0,0 +1,29 @@
+####
+## Notes:
+## * You may need to run this with boa's "conda mambabuild" to correctly resolve the dependencies.
+## * If you are compiling this on a non-cuda machine, to install the right kokkos package, you need to 
+##   set the CONDA_OVERRIDE_CUDA flag to 12.0 . This will mark __cuda (the cuda virtual package) as installed. 
+####
+
+# These flag variables are set by anaconda.
+source "${RECIPE_DIR}/../../add-compiler-flags.sh"
+
+# MPICXX -> nvcc_wrapper -> nvcc -> $GXX
+#           ^ Passes $NVCC_WRAPPER_DEFAULT_COMPILER as the host compiler to nvcc
+# Setting NVCC_WRAPPER_DEFAULT_COMPILER=$GXX enforces that
+# nvcc gets the correct compiler for the target platform.
+export OMPI_CXX=nvcc_wrapper
+export NVCC_WRAPPER_DEFAULT_COMPILER=$GXX
+
+cd src/EVPFFT
+mkdir build
+cd build
+cmake ../src/ \
+    -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+    -D USE_CUFFT=1 \
+    $CMAKE_ARGS \
+    -D CUDAToolkit_ROOT=$PREFIX/bin \
+    -D CMAKE_CXX_COMPILER=$BUILD_PREFIX/bin/mpicxx \
+    -D CMAKE_C_COMPILER=$BUILD_PREFIX/bin/mpicc
+
+make install
