7530a26c3fe3e23edfc755c43693c611b360275a
Kevin Welsh <kwelsh@lanl.gov>
2023-10-04 10:22:27 -0600

    integrate mesh-builder into the CLI

--- src/CLI/include/backend.hpp
+++ src/CLI/include/backend.hpp
@@ -3,11 +3,27 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
+#include <optional>
 #include <filesystem>
 #include "argparse/argparse.hpp"
 #include "argument_exception.hpp"
 
-bool can_exec(const std::filesystem::path& fp) {
+
+/**
+ * Check to see if a file exists on the system.
+ * Technically this will return false if it exists but you don't have permission.
+*/
+inline bool file_exists(std::string fname) {
+    std::fstream f(fname.c_str());
+    return f.good();
+}
+
+inline std::string absolute_fp(std::string fname) {
+    return std::string(std::filesystem::absolute(fname));
+}
+
+inline bool can_exec(const std::filesystem::path& fp) {
     auto entry = std::filesystem::directory_entry(fp);
     return (
         entry.exists()
@@ -16,33 +32,40 @@ bool can_exec(const std::filesystem::path& fp) {
     );
 }
 
+static std::vector<std::filesystem::path>& get_paths() {
+    std::string path = std::getenv("PATH");
+    static std::vector<std::filesystem::path> result;
+    if (result.size() > 0) return result;
+    result.push_back(std::filesystem::canonical("/proc/self/exe").parent_path());
+    result.push_back(std::filesystem::path("."));
+
+    size_t i = 0;
+    // Windows uses ";" as a delimeter, but we don't support windows yet.
+    while ((i = path.find(":")) != std::string::npos) {
+        result.push_back(std::filesystem::path(path.substr(0, i)));
+        path.erase(0, i + 1);
+    }
+    return result;
+}
+
 struct FierroBackend {
     std::string name = "";
     std::shared_ptr<argparse::ArgumentParser> command;
-    FierroBackend(std::string name): name(name) { }
-
-    bool exists() {
-        std::string path = std::getenv("PATH");
-        auto result = std::vector<std::filesystem::path>();
-        result.push_back(std::filesystem::canonical("/proc/self/exe").parent_path());
-        result.push_back(std::filesystem::path("."));
-
-        size_t i = 0;
-        // Windows uses ";" as a delimeter, but we don't support windows yet.
-        while ((i = path.find(":")) != std::string::npos) {
-            result.push_back(std::filesystem::path(path.substr(0, i)));
-            path.erase(0, i + 1);
-        }
-
-        return this->exists(result);
+    std::optional<std::filesystem::path> exec_path{};
+    FierroBackend(std::string name): name(name) {
+        exec_path = find();
     }
 
-    bool exists(const std::vector<std::filesystem::path>& paths) {
+    std::optional<std::filesystem::path> find() {
+        return this->find(get_paths());
+    }
+
+    std::optional<std::filesystem::path> find(const std::vector<std::filesystem::path>& paths) {
         for(const auto & path : paths) {
             auto potential_path = path / this->name;
-            if (can_exec(potential_path)) return true;
+            if (can_exec(potential_path)) return {potential_path};
         }
-        return false;
+        return {};
     }
 
     virtual int invoke() = 0;
