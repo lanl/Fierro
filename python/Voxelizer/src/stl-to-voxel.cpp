38f99c0b8bc629dfea3905e51c254f48acf24247
Kevin Welsh <kwelsh@lanl.gov>
2023-10-24 16:33:14 -0600

    woops remove this test code.

--- python/Voxelizer/src/stl-to-voxel.cpp
+++ python/Voxelizer/src/stl-to-voxel.cpp
@@ -1,9 +1,6 @@
 #include <pybind11/pybind11.h>
 #include "stl-to-voxelvtk.h"
 
-int add(int i, int j) {
-    return i + j;
-}
 
 PYBIND11_MODULE(fierro_voxelizer, m) {
     m.doc() = "backend voxelizer for fierro GUI front-ends.";
