dda4145f19250ace7adbda9d839c82627a1a1795
Kevin Welsh <kwelsh@lanl.gov>
2023-10-24 15:53:01 -0600

    Add standalone evpfft GUI application

---
+++ python/EVPFFT-GUI/evpfft_gui/gui.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+def main():
+    from evpfft_gui.ui_EVPFFT_GUI_V10 import Ui_MainWindow
+    from PySide6.QtWidgets import QMainWindow, QApplication
+    # MAIN WINDOW CLASS
+    class MainWindow(QMainWindow):
+        def __init__(self):
+            QMainWindow.__init__(self)
+            self.ui = Ui_MainWindow()
+            self.ui.setupUi(self)
+            
+            # SHOW WINDOW
+            self.show()
+            
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/EVPFFT-GUI/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = os.path.join(lib_folder, '/requirements.txt')
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path, 'r', encoding='utf-8') as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name='evpfft_gui',
+    python_requires='>=3.8',
+    author='Sarah Hankins',
+    author_email='shankins@lanl.gov',
+    version='0.1',
+    description='Package for running LANL\'s EVPFFT software with a graphical front end.',
+    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=install_requires,
+)
