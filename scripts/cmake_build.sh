8b2eb2a29aa3075672822845fc9808964df19d01
Daniel Dunning <39738037+djdunning@users.noreply.github.com>
2023-11-08 09:02:01 -0700

    scripts should be able to use Elements as a CMake package

--- scripts/cmake_build.sh
+++ scripts/cmake_build.sh
@@ -16,7 +16,7 @@ heffte_build_type="${2}"
 
 # Install Elements
 if [ ! -d "${ELEMENTS_INSTALL_DIR}/lib" ]; then
-    cmake -D CMAKE_INSTALL_PREFIX="$ELEMENTS_INSTALL_DIR" -D Matar_ENABLE_KOKKOS=ON -D Matar_KOKKOS_PACKAGE=Trilinos -B "${ELEMENTS_BUILD_DIR}" -S "${ELEMENTS_SOURCE_DIR}"
+    cmake -D CMAKE_INSTALL_PREFIX="$ELEMENTS_INSTALL_DIR" -D Trilinos_DIR=${TRILINOS_INSTALL_DIR}/lib/cmake/Trilinos -D Matar_ENABLE_KOKKOS=ON -D Matar_KOKKOS_PACKAGE=Trilinos -B "${ELEMENTS_BUILD_DIR}" -S "${ELEMENTS_SOURCE_DIR}"
     make -C "${ELEMENTS_BUILD_DIR}" -j${FIERRO_BUILD_CORES}
     make -C "${ELEMENTS_BUILD_DIR}" install
 fi
