351ddceb912b523595e2e985f8d3329074bc9f00
Kevin Welsh <kevinwelsh132@gmail.com>
2023-11-20 16:07:36 -0700

    use $CC_FOR_BUILD and $CXX_FOR_BUILD instead of $GCC and $GXX to handle clang as well

--- .conda/cross-compile-setup.sh
+++ .conda/cross-compile-setup.sh
@@ -1,8 +1,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/patch_conda_cxxflags.sh"
 # Make sure the MPI wrappers find the right compilers
-export OMPI_CC=$GCC
-export OMPI_CXX=$GXX
+export OMPI_CC=$CC_FOR_BUILD
+export OMPI_CXX=$CXX_FOR_BUILD
 export OMPI_FC=$F90
 
 if [ "$CROSS_COMPILE" = 1 ]; then
