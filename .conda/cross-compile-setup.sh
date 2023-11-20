617a0f0cdb88abc6143050af40d9cf686715646d
Kevin Welsh <kevinwelsh132@gmail.com>
2023-11-20 16:32:21 -0700

    Definitely going crazy if the "CXX_FOR_BUILD" is not the one you should be using "FOR BUILD"...

--- .conda/cross-compile-setup.sh
+++ .conda/cross-compile-setup.sh
@@ -1,9 +1,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/patch_conda_cxxflags.sh"
 # Make sure the MPI wrappers find the right compilers
-export OMPI_CC=$CC_FOR_BUILD
-export OMPI_CXX=$CXX_FOR_BUILD
-export OMPI_FC=$FC_FOR_BUILD
+export OMPI_CC=$CC
+export OMPI_CXX=$CXX
+export OMPI_FC=$FC
 
 if [ "$CROSS_COMPILE" = 1 ]; then
     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${SCRIPT_DIR}/cross-linux.cmake")
