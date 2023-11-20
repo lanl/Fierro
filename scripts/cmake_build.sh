9fa8d3efc06f47ae05fd68dbe52e6883d6bbba9c
Kevin Welsh <kevinwelsh132@gmail.com>
2023-11-20 11:18:49 -0700

    put trilinos_dir before the elements install

--- scripts/cmake_build.sh
+++ scripts/cmake_build.sh
@@ -14,10 +14,16 @@ heffte_build_type="${2}"
 #  git submodule update --init --recursive
 #fi
 
+if [ ! -d "${TRILINOS_INSTALL_DIR}/lib" ]; then
+    Trilinos_DIR=${TRILINOS_INSTALL_DIR}/lib64/cmake/Trilinos
+else
+    Trilinos_DIR=${TRILINOS_INSTALL_DIR}/lib/cmake/Trilinos
+fi
+
 # Install Elements
 if [ ! -d "${ELEMENTS_INSTALL_DIR}/lib" ]; then
     echo "Installing Elements..."
-    cmake -D CMAKE_INSTALL_PREFIX="$ELEMENTS_INSTALL_DIR" -D Trilinos_DIR=${TRILINOS_INSTALL_DIR}/lib64/cmake/Trilinos -D Matar_ENABLE_KOKKOS=ON -D Matar_KOKKOS_PACKAGE=Trilinos -B "${ELEMENTS_BUILD_DIR}" -S "${ELEMENTS_SOURCE_DIR}"
+    cmake -D CMAKE_INSTALL_PREFIX="$ELEMENTS_INSTALL_DIR" -D Trilinos_DIR="${Trilinos_DIR}" -D Matar_ENABLE_KOKKOS=ON -D Matar_KOKKOS_PACKAGE=Trilinos -B "${ELEMENTS_BUILD_DIR}" -S "${ELEMENTS_SOURCE_DIR}"
     make -C "${ELEMENTS_BUILD_DIR}" -j${FIERRO_BUILD_CORES}
     make -C "${ELEMENTS_BUILD_DIR}" install
 fi
@@ -32,16 +38,8 @@ fi
 # Configure EVPFFT using CMake
 cmake_options=(
     -D CMAKE_PREFIX_PATH="$ELEMENTS_INSTALL_DIR"
+    -D Trilinos_DIR="$Trilinos_DIR"
 )
-if [ ! -d "${TRILINOS_INSTALL_DIR}/lib" ]; then
-    cmake_options+=(
-        -D Trilinos_DIR=${TRILINOS_INSTALL_DIR}/lib64/cmake/Trilinos
-    )
-else
-    cmake_options+=(
-        -D Trilinos_DIR=${TRILINOS_INSTALL_DIR}/lib/cmake/Trilinos
-    )
-fi
 
 if [ "$solver" = "all" ]; then
     cmake_options+=(
