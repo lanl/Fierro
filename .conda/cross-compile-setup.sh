91a156fcdc0446cead0fb7dd9fbbc8748352f249
Kevin Welsh <kevinwelsh132@gmail.com>
2023-11-21 09:13:45 -0700

    Add mac build variants for apple silicon. Probably fix python cross platform cpp builds

--- .conda/cross-compile-setup.sh
+++ .conda/cross-compile-setup.sh
@@ -5,6 +5,6 @@ export OMPI_CC=$CC
 export OMPI_CXX=$CXX
 export OMPI_FC=$FC
 
-if [ "$CROSS_COMPILE" = 1 ]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${SCRIPT_DIR}/cross-linux.cmake")
-fi
+# if [ "$CROSS_COMPILE" = 1 ]; then
+#     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${SCRIPT_DIR}/cross-linux.cmake")
+# fi
