6083bb464591a23bf71b79b4cdc2f3d0d3d29e93
yencal <cyenusah@gmail.com>
2023-12-16 20:07:45 -0700

    updated linux-env.sh

--- src/EVPFFT/scripts/machines/linux-env.sh
+++ src/EVPFFT/scripts/machines/linux-env.sh
@@ -9,4 +9,5 @@
 #export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
 #export OMPI_CC=${CC}
 #export OMPI_CXX=${CXX}
+#export HDF5_ROOT=/path/to/where/HDF5/lib/and/include/folders/are/located
 #export FFTW_ROOT=/path/to/where/FFTW/lib/and/include/folders/are/located
