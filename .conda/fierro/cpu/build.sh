090cf990739de76d6ff3e32cff9a4dcdbc1ab1e6
Kevin Welsh <kwelsh@lanl.gov>
2023-12-19 09:00:51 -0700

    add _LIBCPP_DISABLE_AVAILABILITY

--- .conda/fierro/cpu/build.sh
+++ .conda/fierro/cpu/build.sh
@@ -2,6 +2,11 @@
 source "$RECIPE_DIR/../../cross-compile-setup.sh"
 mkdir build
 cd build
+
+# -D _LIBCPP_DISABLE_AVAILABILITY
+# is added to the CXXFLAGS for MacOS builds. 
+# see https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+
 cmake .. \
       -D CMAKE_BUILD_TYPE:STRING=RELEASE \
       -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
@@ -11,7 +16,7 @@ cmake .. \
       -D BUILD_ELEMENTS=OFF \
       -D DISTRIBUTION=On \
       $CMAKE_ARGS \
-      -D CMAKE_CXX_FLAGS="$PATCHED_CXXFLAGS -fopenmp" \
+      -D CMAKE_CXX_FLAGS="$PATCHED_CXXFLAGS -fopenmp -D_LIBCPP_DISABLE_AVAILABILITY" \
       -D MPI_C_COMPILER="$BUILD_PREFIX/bin/mpicc" \
       -D MPI_CXX_COMPILER="$BUILD_PREFIX/bin/mpicxx" \
       -D VECTOR_ARCH_FLAGS="$VECTOR_ARCH_FLAGS" \
