e4bfc3221fc0574b19c37ee2d986a961c089ada3
“Adrian-Diaz” <adriandiaz1117@gmail.com>
2024-01-16 23:47:04 -0700

    sspd singularity

--- src/Parallel-Solvers/Material-Models/Ideal-Gas-Models/IdealGasEOSModel.cpp
+++ src/Parallel-Solvers/Material-Models/Ideal-Gas-Models/IdealGasEOSModel.cpp
@@ -56,7 +56,8 @@ double IdealGasEOSModel::calc_sound_speed_gradient_internal_energy(
   const double den,
   const double sie){
   // sound speed gradient
-  if(elem_sspd(elem_gid) < csmin_)
+  //compute sound speed to decide on avoiding sinfularity near sie=0
+  if(sqrt(gamma_*(gamma_ - 1.0)*sie) < csmin_)
     return 0;
   else
     return 0.5*gamma_*(gamma_ - 1.0)/sqrt(gamma_*(gamma_ - 1.0)*sie);
