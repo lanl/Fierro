978ad8e39931d66a35343fdd869985f70d6d5f9f
shankinsMechEng <134628078+shankinsMechEng@users.noreply.github.com>
2024-04-15 06:59:33 -0600

    clean developer inputs file

--- python/FIERRO-GUI/fierro_gui/DeveloperInputs.py
+++ python/FIERRO-GUI/fierro_gui/DeveloperInputs.py
@@ -1,4 +1,4 @@
-fierro_mesh_builder_exe = '/Users/shankins/Documents/FY24/Github/XcodeFierro/Fierro/build-fierro-serial/bin/fierro-mesh-builder'
-fierro_voxelizer_exe = '/Users/shankins/Documents/FY24/Github/XcodeFierro/Fierro/build-fierro-serial/bin/fierro-voxelizer'
-fierro_parallel_explicit_exe = '/Users/shankins/Documents/FY24/Github/XcodeFierro/Fierro/build-fierro-serial/bin/fierro-parallel-explicit'
-fierro_evpfft_exe = '/Users/shankins/Documents/FY24/Github/XcodeFierro/Fierro/src/EVPFFT/evpfft_fftw_serial/evpfft'
+fierro_mesh_builder_exe = '\'
+fierro_voxelizer_exe = '\'
+fierro_parallel_explicit_exe = '\'
+fierro_evpfft_exe = '\'
