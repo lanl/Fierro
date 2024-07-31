991736c161fb4534b80cbcab4cb591695c73e63e
Daniel Dunning <39738037+djdunning@users.noreply.github.com>
2024-07-31 12:33:48 -0600

    continuing heffte cuda

--- .conda/heffte/cuda/build.sh
+++ .conda/heffte/cuda/build.sh
@@ -11,11 +11,11 @@ fi
 
 source "$RECIPE_DIR/../../cross-compile-setup.sh"
 
-export OMPI_CXX=nvcc
+#export OMPI_CXX=nvcc
 # Only do this for cross compiling
-if [ "$PLATFORM" != "linux-64" ] ; then
-    export NVCC_PREPEND_FLAGS="-ccbin $CXX";
-fi
+#if [ "$PLATFORM" != "linux-64" ] ; then
+#    export NVCC_PREPEND_FLAGS="-ccbin $CXX";
+#fi
 # These things need to be set for certain cmake versions.
 # The cmake CUDA compiler tests need to see the host env libs
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH $PREFIX/lib"
@@ -30,10 +30,10 @@ cmake -D CMAKE_BUILD_TYPE:STRING=RELEASE \
       $CMAKE_ARGS \
       $SRC_DIR \
       -D CMAKE_CXX_FLAGS="$PATCHED_CXXFLAGS" \
-      -D MPI_C_COMPILER="$BUILD_PREFIX/bin/mpicc" \
-      -D MPI_CXX_COMPILER="$BUILD_PREFIX/bin/mpicxx" \
-      -D CMAKE_CUDA_HOST_LINK_LAUNCHER=$CXX \
-      -D CMAKE_CUDA_COMPILER=$(which nvcc) \
+ #     -D MPI_C_COMPILER="$BUILD_PREFIX/bin/mpicc" \
+ #     -D MPI_CXX_COMPILER="$BUILD_PREFIX/bin/mpicxx" \
+#      -D CMAKE_CUDA_HOST_LINK_LAUNCHER=$CXX \
+#      -D CMAKE_CUDA_COMPILER=$(which nvcc) \
 
 make -j 10 install
 
