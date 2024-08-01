9ae44bbb0d07923532d9f106217f7d1f4e34757e
Daniel Dunning <39738037+djdunning@users.noreply.github.com>
2024-08-01 10:29:44 -0600

    continuing heffte cuda

--- .conda/heffte/cuda/build.sh
+++ .conda/heffte/cuda/build.sh
@@ -21,9 +21,6 @@ source "$RECIPE_DIR/../../cross-compile-setup.sh"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH $PREFIX/lib"
 export LIBRARIES="$LIBRARIES \"-L$PREFIX/lib\""
 
-#      -D CMAKE_CUDA_HOST_LINK_LAUNCHER=$CXX \
-#      -D MPI_C_COMPILER="$BUILD_PREFIX/bin/mpicc" \
-#      -D MPI_CXX_COMPILER="$BUILD_PREFIX/bin/mpicxx" \
 cmake -D CMAKE_BUILD_TYPE:STRING=RELEASE \
       -D BUILD_SHARED_LIBS=ON \
       -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
@@ -31,6 +28,9 @@ cmake -D CMAKE_BUILD_TYPE:STRING=RELEASE \
       -D Heffte_ENABLE_CUDA=ON \
       -D Heffte_DISABLE_GPU_AWARE_MPI=ON \
       -D CMAKE_CXX_FLAGS="$PATCHED_CXXFLAGS" \
+      -D CMAKE_CUDA_HOST_LINK_LAUNCHER=$CXX \
+      -D MPI_C_COMPILER="$BUILD_PREFIX/bin/mpicc" \
+      -D MPI_CXX_COMPILER="$BUILD_PREFIX/bin/mpicxx" \
       -D CMAKE_CUDA_COMPILER='which nvcc' \
       $CMAKE_ARGS \
       -S $SRC_DIR \
