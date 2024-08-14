031934475223fb2f9253fbda00c02e471cb0affd
Jacob Moore <jacoblinleymoore@gmail.com>
2024-08-14 11:44:40 -0500

    ENH: Adding test for reading ensight mesh

--- single-node-refactor/regression_tests/test_refactor.py
+++ single-node-refactor/regression_tests/test_refactor.py
@@ -13,7 +13,7 @@ builds = ["openmp"]
 solvers = ["Fierro"]
 
 # Add names of each test
-tests = ["Sedov", "Sod_X", "Sod_Y", "Sod_Z", "Sedov_Erosion"]
+tests = ["Sedov", "Sod_X", "Sod_Y", "Sod_Z", "Sedov_Erosion","Sedov_Read_Ensight"]
 
 # Extract data from txt file
 def extract_state_data(filename):
