03bd8b6a65f1d24a0004ad9a9f8b5e10a3bac7ec
“Adrian-Diaz” <adriandiaz1117@gmail.com>
2024-08-15 12:18:45 -0600

    WIP: refactor adjoint for generic objective

--- src/Parallel-Solvers/Optimization_Modules/Fierro_Optimization_Objective.hpp
+++ src/Parallel-Solvers/Optimization_Modules/Fierro_Optimization_Objective.hpp
@@ -137,13 +137,22 @@ public:
   
   //NOTE: these functions assign for every component of the gradient vector
   //contributes to rate of change of adjoint vector due to term with velocity gradient of objective
-  virtual void velocity_gradient_adjoint_contribution(vec_array& adjoint_rate_vector){}
+  virtual void velocity_gradient_adjoint_contribution(vec_array& adjoint_rate_vector, const DViewCArrayKokkos<double>& node_mass,
+                                                      const DViewCArrayKokkos<double>& elem_mass, const DViewCArrayKokkos<double>& node_vel,
+                                                      const DViewCArrayKokkos<double>& node_coords, const DViewCArrayKokkos<double>& elem_sie,
+                                                      const size_t& rk_level){}
 
   //contributes to rate of change of adjoint vector due to term with displacement gradient of objective
-  virtual void displacement_gradient_adjoint_contribution(vec_array& adjoint_rate_vector){}
+  virtual void displacement_gradient_adjoint_contribution(vec_array& adjoint_rate_vector, const DViewCArrayKokkos<double>& node_mass,
+                                                      const DViewCArrayKokkos<double>& elem_mass, const DViewCArrayKokkos<double>& node_vel,
+                                                      const DViewCArrayKokkos<double>& node_coords, const DViewCArrayKokkos<double>& elem_sie,
+                                                      const size_t& rk_level){}
 
   //contributes to rate of change of adjoint vector due to term with specific internal energy gradient of objective
-  virtual void sie_gradient_adjoint_contribution(vec_array& adjoint_rate_vector){}
+  virtual void sie_gradient_adjoint_contribution(vec_array& adjoint_rate_vector, const DViewCArrayKokkos<double>& node_mass,
+                                                      const DViewCArrayKokkos<double>& elem_mass, const DViewCArrayKokkos<double>& node_vel,
+                                                      const DViewCArrayKokkos<double>& node_coords, const DViewCArrayKokkos<double>& elem_sie,
+                                                      const size_t& rk_level){}
 
 
 }; // class ObjectiveMMA 
