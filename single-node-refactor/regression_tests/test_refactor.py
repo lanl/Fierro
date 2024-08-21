55d0f134b6b492d70c2ad05e1b95bcbc21e54c7f
Nathaniel Morgan <47866137+nathanielmorgan@users.noreply.github.com>
2024-08-21 09:05:46 -0600

    Add Andrew Morgan's fix to regression script to ensure file times aligns with the reference files (#221)
    Co-authored-by: Nathaniel Morgan <nmorgan@syrupcastle.lanl.gov>

--- single-node-refactor/regression_tests/test_refactor.py
+++ single-node-refactor/regression_tests/test_refactor.py
@@ -72,8 +72,14 @@ for i in range(len(executables)):
 
         # Compare to standard results
         pattern = "state/mat_pt_state*"
-        file = glob.glob(pattern)
-        file_path = file[-1]
+        files = glob.glob(pattern)
+        
+        try: fileIndex = list(file.split("/")[-1] for file in files).index(standard_results[j].split("/")[-1])
+        except ValueError:
+            print("state file in test not found\n")
+            exit()  # no solution
+
+        file_path = files[fileIndex]
 
         print(file_path)
         print(standard_results[j])
@@ -86,12 +92,15 @@ for i in range(len(executables)):
             true = [row[k] for row in standard_data]
 
 
+
             for l in range(len(calc)):
                 diff = calc[l] - true[l]
                 # print(diff)
 
                 if abs(diff) > 1E-11:
                     print(diff)
+                    print(calc[l])
+                    print(true[l])
                     raise Exception("Results do not match for "+header1[k]+" for the "+tests[j]+" test!")
 
         # Remove simulated state dump
