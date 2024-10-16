4ebe931b01302e1d457e975213c3032d5f0dc048
Nathaniel Morgan <nmorgan@syrupcastle.lanl.gov>
2024-10-16 08:33:32 -0600

    added host pointer to eos models

--- single-node-refactor/src/input/parse_yaml.cpp
+++ single-node-refactor/src/input/parse_yaml.cpp
@@ -1690,6 +1690,8 @@ void parse_materials(Yaml::Node& root, Material_t& Materials)
                                 Materials.MaterialFunctions(mat_id).calc_pressure    = &NoEOSModel::calc_pressure;
                                 Materials.MaterialFunctions(mat_id).calc_sound_speed = &NoEOSModel::calc_sound_speed;
                             });
+                            Materials.MaterialFunctions.host(mat_id).calc_pressure    = &NoEOSModel::calc_pressure;
+                            Materials.MaterialFunctions.host(mat_id).calc_sound_speed = &NoEOSModel::calc_sound_speed;
                             if (VERBOSE) {
                                 std::cout << "\teos_model = " << eos << std::endl;
                             }
@@ -1700,6 +1702,8 @@ void parse_materials(Yaml::Node& root, Material_t& Materials)
                                 Materials.MaterialFunctions(mat_id).calc_pressure    = &GammaLawGasEOSModel::calc_pressure;
                                 Materials.MaterialFunctions(mat_id).calc_sound_speed = &GammaLawGasEOSModel::calc_sound_speed;
                             });
+                                Materials.MaterialFunctions.host(mat_id).calc_pressure    = &GammaLawGasEOSModel::calc_pressure;
+                                Materials.MaterialFunctions.host(mat_id).calc_sound_speed = &GammaLawGasEOSModel::calc_sound_speed;
                             if (VERBOSE) {
                                 std::cout << "\teos_model = " << eos << std::endl;
                             }
