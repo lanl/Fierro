2f4cabb844da38f31410755966f4cfa378d87cd4
Daniel Dunning <djdunning1356@gmail.com>
2025-02-24 10:36:41 -0700

    trying to fix elements package

--- .conda/elements/cpu/build.sh
+++ .conda/elements/cpu/build.sh
@@ -9,13 +9,14 @@ if [ $(uname) == Linux ]; then
     export MPI_FLAGS="$MPI_FLAGS;-mca;plm;isolated"
 fi
 
+      #-D Matar_ENABLE_KOKKOS=ON \
+
 source "$RECIPE_DIR/../../cross-compile-setup.sh"
 
 cmake -D CMAKE_BUILD_TYPE:STRING=RELEASE \
       -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
       -D CMAKE_CXX_STANDARD:STRING=17 \
       -D VECTOR_ARCH_FLAGS="$VECTOR_ARCH_FLAGS" \
-      -D Matar_ENABLE_KOKKOS=ON \
       $CMAKE_ARGS \
       $SRC_DIR \
       -D CMAKE_CXX_FLAGS="$PATCHED_CXXFLAGS" \
