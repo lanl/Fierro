fe7feb257bd67e648cd21c14b79256f3e08c0be3
Nathaniel Morgan <nmorgan@syrupcastle.lanl.gov>
2025-04-17 10:38:01 -0600

    added level set solver folders and files

---
+++ single-node-refactor/src/Solvers/level_set_solver/include/level_set_solver.h
@@ -0,0 +1,240 @@
+/**********************************************************************************************
+ © 2020. Triad National Security, LLC. All rights reserved.
+ This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos
+ National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S.
+ Department of Energy/National Nuclear Security Administration. All rights in the program are
+ reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear
+ Security Administration. The Government is granted for itself and others acting on its behalf a
+ nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare
+ derivative works, distribute copies to the public, perform publicly and display publicly, and
+ to permit others to do so.
+ This program is open source under the BSD-3 License.
+ Redistribution and use in source and binary forms, with or without modification, are permitted
+ provided that the following conditions are met:
+ 1.  Redistributions of source code must retain the above copyright notice, this list of
+ conditions and the following disclaimer.
+ 2.  Redistributions in binary form must reproduce the above copyright notice, this list of
+ conditions and the following disclaimer in the documentation and/or other materials
+ provided with the distribution.
+ 3.  Neither the name of the copyright holder nor the names of its contributors may be used
+ to endorse or promote products derived from this software without specific prior
+ written permission.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **********************************************************************************************/
+
+#ifndef LVLSET_SOLVER_H
+#define LVLSET_SOLVER_H
+
+#include "solver.h"
+#include "state.h"
+
+// Forward declare structs
+struct SimulationParameters_t;
+struct Material_t;
+struct Mesh_t;
+struct BoundaryCondition_t;
+struct RegionFill_t;
+struct RegionFill_host_t;
+
+
+using namespace mtr; // matar namespace
+
+namespace LevelSet_State
+{
+    // Node state to be initialized for the SGH solver
+    static const std::vector<node_state> required_node_state = 
+    {
+        node_state::coords, 
+        node_state::gradient_level_set,
+        node_state::velocity,
+        node_state::volume
+    };
+
+    // Gauss point state to be initialized for the SGH solver
+    static const std::vector<gauss_pt_state> required_gauss_pt_state = 
+    { 
+        gauss_pt_state::level_set,
+        gauss_pt_state::volume,
+    };
+
+    // Material point state to be initialized for the SGH solver
+    static const std::vector<material_pt_state> required_material_pt_state =
+    {
+        material_pt_state::volume_fraction,
+    }; 
+    // nothing is needed on material pt state index space
+
+} // end namespace
+
+/////////////////////////////////////////////////////////////////////////////
+///
+/// \class LevelSet
+///
+/// \brief Class for containing functions required to evolve levelset fields
+///
+/// This class contains the requisite functions requited to evolve
+/// a levelset field
+///
+/////////////////////////////////////////////////////////////////////////////
+class LevelSet : public Solver
+{
+public:
+
+    LevelSet()  : Solver()
+    {
+    }
+
+    ~LevelSet() = default;
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///
+    /// \fn Initialize
+    ///
+    /// \brief Initializes data associated with the LevelSet solver
+    ///
+    /////////////////////////////////////////////////////////////////////////////
+    void initialize(SimulationParameters_t& SimulationParamaters, 
+                    Material_t& Materials, 
+                    Mesh_t& mesh, 
+                    BoundaryCondition_t& Boundary,
+                    State_t& State) const override;
+
+    void initialize_material_state(SimulationParameters_t& SimulationParamaters, 
+        Material_t& Materials, 
+        Mesh_t& mesh, 
+        BoundaryCondition_t& Boundary,
+        State_t& State) const override;
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///
+    /// \fn setup
+    ///
+    /// \brief Calls setup_sgh_rz, which initializes state and material data
+    ///
+    /////////////////////////////////////////////////////////////////////////////
+    void setup(SimulationParameters_t& SimulationParamaters, 
+               Material_t& Materials, 
+               Mesh_t& mesh, 
+               BoundaryCondition_t& Boundary,
+               State_t& State) override;
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///
+    /// \fn execute
+    ///
+    /// \brief Calls the solve function which evolves the state
+    ///
+    ///
+    /////////////////////////////////////////////////////////////////////////////
+    void execute(SimulationParameters_t& SimulationParamaters, 
+                 Material_t& Materials, 
+                 BoundaryCondition_t& Boundary, 
+                 Mesh_t& mesh, 
+                 State_t& State) override;
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///
+    /// \fn finalize
+    ///
+    /// \brief <insert brief description>
+    ///
+    /// <Insert longer more detailed description which
+    /// can span multiple lines if needed>
+    ///
+    /// \param <function parameter description>
+    /// \param <function parameter description>
+    /// \param <function parameter description>
+    ///
+    /// \return <return type and definition description if not void>
+    ///
+    /////////////////////////////////////////////////////////////////////////////
+    void finalize(SimulationParameters_t& SimulationParamaters, 
+                  Material_t& Materials, 
+                  BoundaryCondition_t& Boundary) const override
+    {
+        // Any finalize goes here, remove allocated memory, etc
+    }
+
+                        
+
+
+    // **** Functions defined in properties.cpp **** //
+    void update_lvlset(
+        const Material_t& Materials,
+        const Mesh_t& mesh,
+        const DCArrayKokkos<double>& node_coords,
+        const DCArrayKokkos<double>& node_vel,
+        const DCArrayKokkos<double>& GaussPoints_vel_grad,
+        const DCArrayKokkos<double>& MaterialPoints_den,
+        const DCArrayKokkos<double>& MaterialPoints_pres,
+        const DCArrayKokkos<double>& MaterialPoints_stress,
+        const DCArrayKokkos<double>& MaterialPoints_stress_n0,
+        const DCArrayKokkos<double>& MaterialPoints_sspd,
+        const DCArrayKokkos<double>& MaterialPoints_sie,
+        const DCArrayKokkos<double>& GaussPoints_vol,
+        const DCArrayKokkos<double>& MaterialPoints_mass,
+        const DCArrayKokkos<double>& MaterialPoints_eos_state_vars,
+        const DCArrayKokkos<double>& MaterialPoints_strength_state_vars,
+        const DCArrayKokkos<bool>&   MaterialPoints_eroded,
+        const DCArrayKokkos<double>& MaterialPoints_shear_modulii,
+        const DCArrayKokkos<size_t>& MaterialToMeshMaps_elem,
+        const double time_value,
+        const double dt,
+        const double rk_alpha,
+        const size_t cycle,
+        const size_t num_material_elems,
+        const size_t mat_id) const;
+
+
+
+
+    // **** Functions defined in time_integration.cpp **** //
+    // NOTE: Consider pulling up
+    void rk_init_rz(
+        DCArrayKokkos<double>& node_coords,
+        DCArrayKokkos<double>& node_coords_n0,
+        DCArrayKokkos<double>& node_vel,
+        DCArrayKokkos<double>& node_vel_n0,
+        DCArrayKokkos<double>& MaterialPoints_sie,
+        DCArrayKokkos<double>& MaterialPoints_sie_n0,
+        DCArrayKokkos<double>& MaterialPoints_stress,
+        DCArrayKokkos<double>& MaterialPoints_stress_n0,
+        const size_t num_dims,
+        const size_t num_elems,
+        const size_t num_nodes,
+        const size_t num_mat_points) const;
+
+
+    void get_timestep_rz(
+        Mesh_t& mesh,
+        DCArrayKokkos<double>& node_coords,
+        DCArrayKokkos<double>& node_vel,
+        DCArrayKokkos<double>& GaussPoints_vol,
+        DCArrayKokkos<double>& MaterialPoints_sspd,
+        DCArrayKokkos<bool>&   MaterialPoints_eroded,
+        DCArrayKokkos<size_t>& MaterialToMeshMaps_elem,
+        size_t num_mat_elems,
+        double time_value,
+        const double graphics_time,
+        const double time_final,
+        const double dt_max,
+        const double dt_min,
+        const double dt_cfl,
+        double&      dt,
+        const double fuzz) const;
+
+
+}; // end class
+
+
+#endif // end Header Guard
---
+++ single-node-refactor/src/Solvers/level_set_solver/src/level_set_execute.cpp
@@ -0,0 +1,251 @@
+/**********************************************************************************************
+© 2020. Triad National Security, LLC. All rights reserved.
+This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos
+National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S.
+Department of Energy/National Nuclear Security Administration. All rights in the program are
+reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear
+Security Administration. The Government is granted for itself and others acting on its behalf a
+nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare
+derivative works, distribute copies to the public, perform publicly and display publicly, and
+to permit others to do so.
+This program is open source under the BSD-3 License.
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+1.  Redistributions of source code must retain the above copyright notice, this list of
+conditions and the following disclaimer.
+2.  Redistributions in binary form must reproduce the above copyright notice, this list of
+conditions and the following disclaimer in the documentation and/or other materials
+provided with the distribution.
+3.  Neither the name of the copyright holder nor the names of its contributors may be used
+to endorse or promote products derived from this software without specific prior
+written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************************************/
+
+#include "level_set_solver.h"
+
+#include "simulation_parameters.h"
+#include "material.h"
+#include "boundary_conditions.h"
+#include "mesh.h"
+#include "state.h"
+#include "geometry_new.h"
+#include "mesh_io.h"
+
+/////////////////////////////////////////////////////////////////////////////
+///
+/// \fn solve
+///
+/// Evolve the state according to the SGH method
+///
+/////////////////////////////////////////////////////////////////////////////
+void LevelSet::execute(SimulationParameters_t& SimulationParamaters, 
+                    Material_t& Materials, 
+                    BoundaryCondition_t& BoundaryConditions, 
+                    Mesh_t& mesh, 
+                    State_t& State)
+{
+
+    double fuzz  = SimulationParamaters.dynamic_options.fuzz;
+    // double tiny  = SimulationParamaters.dynamic_options.tiny;
+    double small = SimulationParamaters.dynamic_options.small;
+
+    double graphics_dt_ival  = SimulationParamaters.output_options.graphics_time_step;
+    int    graphics_cyc_ival = SimulationParamaters.output_options.graphics_iteration_step;
+
+    // double time_initial = SimulationParamaters.dynamic_options.time_initial;
+    double time_final   = this->time_end; //SimulationParamaters.dynamic_options.time_final;
+    double dt_min   = SimulationParamaters.dynamic_options.dt_min;
+    double dt_max   = SimulationParamaters.dynamic_options.dt_max;
+    double dt_start = SimulationParamaters.dynamic_options.dt_start;
+    double dt_cfl   = SimulationParamaters.dynamic_options.dt_cfl;
+
+    int rk_num_stages = SimulationParamaters.dynamic_options.rk_num_stages;
+    int cycle_stop    = SimulationParamaters.dynamic_options.cycle_stop;
+
+    // initialize time, time_step, and cycles
+    double time_value = this->time_start;  // was 0.0
+    double dt = dt_start;
+
+    // Create mesh writer
+    MeshWriter mesh_writer; // Note: Pull to driver after refactoring evolution
+
+    // --- Graphics vars ----
+    CArray<double> graphics_times = CArray<double>(20000);
+    graphics_times(0) = this->time_start; // was zero
+    double graphics_time = this->time_start; // the times for writing graphics dump, was started at 0.0
+
+    std::cout << "Applying initial boundary conditions" << std::endl;
+    //boundary_velocity(mesh, BoundaryConditions, State.node.vel, time_value); // Time value = 0.0;
+
+    
+    double cached_pregraphics_dt = fuzz;
+
+    // the number of materials specified by the user input
+    const size_t num_mats = Materials.num_mats;
+
+    
+
+    // a flag to exit the calculation
+    size_t stop_calc = 0;
+
+    auto time_1 = std::chrono::high_resolution_clock::now();
+
+    // Write initial state at t=0
+    printf("Writing outputs to file at %f \n", graphics_time);
+    mesh_writer.write_mesh(
+        mesh, 
+        State, 
+        SimulationParamaters,
+        dt, 
+        time_value, 
+        graphics_times,
+        LevelSet_State::required_node_state,
+        LevelSet_State::required_gauss_pt_state,
+        LevelSet_State::required_material_pt_state,
+        this->solver_id);
+    
+
+
+    graphics_time = time_value + graphics_dt_ival;
+
+    // loop over the max number of time integration cycles
+    for (size_t cycle = 0; cycle < cycle_stop; cycle++) {
+        // stop calculation if flag
+        if (stop_calc == 1) {
+            break;
+        }
+
+        cached_pregraphics_dt = dt;
+
+        // the smallest time step across all materials
+        double min_dt_calc = dt_max;
+
+        // calculating time step per material
+        for(size_t mat_id = 0; mat_id < num_mats; mat_id++){
+
+            // initialize the material dt
+            double dt_mat = dt;
+
+            // get the stable time step
+            //get_timestep(mesh,
+            //             State.node.coords,
+            //             State.node.vel,
+            //             State.GaussPoints.vol,
+            //             State.MaterialPoints(mat_id).sspd,
+            //             State.MaterialPoints(mat_id).eroded,
+            //             State.MaterialToMeshMaps(mat_id).elem,
+            //             State.MaterialToMeshMaps(mat_id).num_material_elems,
+            //             time_value,
+            //             graphics_time,
+            //             time_final,
+            //             dt_max,
+            //             dt_min,
+            //             dt_cfl,
+            //             dt_mat,
+            //             fuzz);
+
+            // save the smallest dt of all materials
+            min_dt_calc = fmin(dt_mat, min_dt_calc);
+        } // end for loop over all mats
+
+        dt = min_dt_calc;  // save this dt time step
+
+        if (cycle == 0) {
+            printf("cycle = %lu, time = %f, time step = %f \n", cycle, time_value, dt);
+        }
+        // print time step every 10 cycles
+        else if (cycle % 20 == 0) {
+            printf("cycle = %lu, time = %f, time step = %f \n", cycle, time_value, dt);
+        } // end if
+
+
+        // ---------------------------------------------------------------------
+        //  integrate the solution forward to t(n+1) via Runge Kutta (RK) method
+        // ---------------------------------------------------------------------
+        for(size_t mat_id = 0; mat_id < num_mats; mat_id++){
+            // save the values at t_n
+            //rk_init(State.node.coords,
+            //        State.node.coords_n0,
+            //        State.node.vel,
+            //        State.node.vel_n0,
+            //        State.MaterialPoints(mat_id).sie,
+            //        State.MaterialPoints(mat_id).sie_n0,
+            //        State.MaterialPoints(mat_id).stress,
+            //        State.MaterialPoints(mat_id).stress_n0,
+            //        mesh.num_dims,
+            //        mesh.num_elems,
+            //        mesh.num_nodes,
+            //        State.MaterialPoints(mat_id).num_material_points);
+        } // end for mat_id
+
+
+        // integrate solution forward in time
+        for (size_t rk_stage = 0; rk_stage < rk_num_stages; rk_stage++) {
+            
+            // ---- RK coefficient ----
+            double rk_alpha = 1.0 / ((double)rk_num_stages - (double)rk_stage);
+
+
+        } // end of RK loop
+
+        // increment the time
+        time_value += dt;
+
+        size_t write = 0;
+        if ((cycle + 1) % graphics_cyc_ival == 0 && cycle > 0) {
+            write = 1;
+        }
+        else if (cycle == cycle_stop) {
+            write = 1;
+        }
+        else if (time_value >= time_final) {
+            write = 1;
+        }
+        else if (time_value >= graphics_time) {
+            write = 1;
+        }
+
+        // write outputs
+        if (write == 1) {
+            printf("Writing outputs to file at %f \n", graphics_time);
+            mesh_writer.write_mesh(mesh,
+                                   State,
+                                   SimulationParamaters,
+                                   dt,
+                                   time_value,
+                                   graphics_times,
+                                   LevelSet_State::required_node_state,
+                                   LevelSet_State::required_gauss_pt_state,
+                                   LevelSet_State::required_material_pt_state,
+                                   this->solver_id);
+
+            graphics_time = time_value + graphics_dt_ival;
+
+            dt = cached_pregraphics_dt;
+        } // end if
+
+        // end of calculation
+        if (time_value >= time_final) {
+            break;
+        }
+    } // end for cycle loop
+
+    auto time_2    = std::chrono::high_resolution_clock::now();
+    auto calc_time = std::chrono::duration_cast<std::chrono::nanoseconds>(time_2 - time_1).count();
+
+    printf("\nCalculation time in seconds: %f \n", calc_time * 1e-9);
+
+} // end of SGH execute
+
+
