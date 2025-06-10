3aa782cc6f10644e43f2d71036e2f1c35823849b
Nathaniel Morgan <nmorgan@pn2303395.lanl.gov>
2025-06-10 10:10:32 -0600

    WIP: advection data structures

--- single-node-refactor/src/remap/src/remap.cpp
+++ single-node-refactor/src/remap/src/remap.cpp
@@ -38,3 +38,143 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "material.h"
 #include "state.h"
 
+// NOTES:
+// Option1: purely cell centric
+//   loop over all patches in mesh
+//   get elem_gid1 and elem_gid2 for a patch
+//   loop over mats in these elems
+//   use reverse map to find mat_storage_lid_1 and mat_storage_lid_2 for given mat_id
+//   advect all vars for that material (requires atomics)
+
+// Option2: material centric
+//    loop over all mat_elems
+//    loop over the patches
+//    get neighbor elem_gid
+//    find which bin my mat_id is in
+//    advect all vars for that material (requires atomics)
+
+// Option3: material centric patch (best option)
+//    loop over all mat_patches
+//    get neighboring elem_gids
+//    find which bin the mat_id is in
+//    advect all vars for that material (requires atomics)
+
+// ----
+//   saving fluxes to patches and doing a second loop over elements and 
+//   patches in elem is thread safe
+
+
+
+void advect_mat_var(const Mesh_t& mesh,
+                    const DCArrayKokkos<double>& node_coords,     // coords after mesh smoothing
+                    const DCArrayKokkos<double>& node_coords_n0,  // coords_n0 are prior to remap
+                    const DRaggedRightArrayKokkos<double>& MaterialPoints_den, 
+                    const DRaggedRightArrayKokkos<double>& MaterialPoints_sie, 
+                    const DRaggedRightArrayKokkos<double>& MaterialPoints_stress, 
+                    const DRaggedRightArrayKokkos<double>& MaterialPoints_volfrac,     // a remapped var
+                    const DRaggedRightArrayKokkos<double>& MaterialPoints_geo_volfrac, // interface recon
+                    const DRaggedRightArrayKokkos<size_t>& MaterialToMeshMaps_elem,
+                    const DRaggedRightArrayKokkos<size_t>& MeshToMaterialMaps_mat_id,
+                    const DRaggedRightArrayKokkos<size_t>& MeshToMaterialMaps_mat_storage_lid,
+                    const DCArrayKokkos<size_t>& MeshToMaterialMaps_num_mats_in_elem,
+                    const DCArrayKokkos<size_t>& MaterialToMeshMaps_patches,
+                    const size_t num_mat_patches,
+                    const size_t mat_id,
+                    const double fuzz,
+                    const double small)
+    {
+
+        // Loop over the patches on the mesh
+        FOR_ALL(mat_patch_lid, 0, num_mat_patches, {
+
+            // get patches gid
+            size_t patch_gid = MaterialToMeshMaps_patches(mat_id, mat_patch_lid); 
+
+            // get elem gids
+            const size_t elem_gid_0 = mesh.elems_in_patch(patch_gid, 0); 
+            const size_t elem_gid_1 = mesh.elems_in_patch(patch_gid, 1);
+            
+            // the material elem storage index
+            size_t mat_elem_lid_0;
+            size_t mat_elem_lid_1;
+
+            // loop over mat_lid in elem_gid_0
+            for (size_t mat_lid=0; mat_lid<MeshToMaterialMaps_num_mats_in_elem(elem_gid_0); mat_lid++){
+                if (MeshToMaterialMaps_mat_id(elem_gid_0, mat_lid)==mat_id){
+                    mat_elem_lid_0 = MeshToMaterialMaps_mat_storage_lid(elem_gid_0, mat_lid);
+                } // end if
+            } // end for mat_lid
+
+            // loop over mat_lid in elem_gid_1
+            for (size_t mat_lid=0; mat_lid<MeshToMaterialMaps_num_mats_in_elem(elem_gid_1); mat_lid++){
+                if (MeshToMaterialMaps_mat_id(elem_gid_1, mat_lid)==mat_id){
+                    mat_elem_lid_1 = MeshToMaterialMaps_mat_storage_lid(elem_gid_1, mat_lid);
+                } // end if
+            } // end for mat_lid
+
+
+            // --- calculate surfaced area ---
+            double avg_coords[3];
+            avg_coords[0] = 0.0;
+            avg_coords[1] = 0.0;
+            avg_coords[2] = 0.0;
+            for (size_t node_lid=0; node_lid<mesh.num_nodes_in_patch; node_lid++){
+                
+                // get the node id
+                size_t node_gid = mesh.nodes_in_patch(patch_gid, node_lid);
+
+
+                for (size_t dim=0; dim<mesh.num_dims; dim++){
+                    avg_coords[dim] += node_coords(node_gid, dim);
+                } // end for dim
+
+            } // end for
+
+            for (size_t dim=0; dim<mesh.num_dims; dim++){
+                avg_coords[dim] /= (double)mesh.num_nodes_in_patch;
+            } // end for dim
+
+
+            // allocate the corner surface normals and forces 
+            double corn_patch_area_normal_1D[3];
+            ViewCArrayKokkos <double> corn_patch_area_normal(&corn_patch_area_normal_1D[0],3);
+            
+            // initialize corner patch vector to zero
+            for (size_t dim=0; dim<mesh.num_dims; dim++){
+                corn_patch_area_normal(dim) = 0.0;
+            } // end for dim
+
+            double vec_a[3];
+            double vec_b[3];
+            for (size_t node_lid=0; node_lid<mesh.num_nodes_in_patch; node_lid++){
+                
+                // get the node ids for the triangle
+                size_t node_gid_0 = mesh.nodes_in_patch(patch_gid, node_lid);
+                size_t node_gid_1;
+                if(node_lid<mesh.num_nodes_in_patch-1){
+                    node_gid_1 = mesh.nodes_in_patch(patch_gid, node_lid+1);
+                } else {
+                    node_gid_1 = mesh.nodes_in_patch(patch_gid, 0);
+                } // end if
+        
+                for (size_t dim=0; dim<mesh.num_dims; dim++){
+                    vec_a[dim] = node_coords(node_gid_0, dim) - avg_coords[dim];
+                    vec_b[dim] = node_coords(node_gid_1, dim) - avg_coords[dim];
+                } // end for dim
+
+                // calculating the cross product of the 2 vectors, 1/2 multiply is later
+                corn_patch_area_normal(0) += (vec_a[1]*vec_b[2] - vec_a[2]*vec_b[1]);
+                corn_patch_area_normal(1) += (vec_a[2]*vec_b[0] - vec_a[0]*vec_b[2]);
+                corn_patch_area_normal(2) += (vec_a[0]*vec_b[1] - vec_a[1]*vec_b[0]);
+
+            } // end for node_lid in patch
+
+            // each node gets 1/4 of the surface area
+            for (size_t dim=0; dim<mesh.num_dims; dim++){
+                corn_patch_area_normal(dim) /= 8;  //  comes from 1/2 * 1/4
+            } // end for dim
+
+        });
+
+    } // end advect function
+
