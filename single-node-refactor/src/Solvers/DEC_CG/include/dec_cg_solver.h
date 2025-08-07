6d2ae1900db7b51514eb213c7c8acb6bd27a444c
Jacob Moore <jacoblinleymoore@gmail.com>
2025-08-07 15:44:41 -0500

    ENH: Initial setup, WIP

---
+++ single-node-refactor/src/Solvers/DEC_CG/include/dec_cg_solver.h
@@ -0,0 +1,221 @@
+/**********************************************************************************************
+© 2020. Triad National Security, LLC. All rights reserved.
+This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos
+National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S.
+Department of Energy/National Nuclear Security Administration. All rights in the program are
+reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear
+Security Administration. The Government is granted for itself and others acting on its behalf a
+nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare
+derivative works, distribute copies to the public, perform publicly and display publicly, and
+to permit others to do so.
+This program is open source under the BSD-3 License.
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+1.  Redistributions of source code must retain the above copyright notice, this list of
+conditions and the following disclaimer.
+2.  Redistributions in binary form must reproduce the above copyright notice, this list of
+conditions and the following disclaimer in the documentation and/or other materials
+provided with the distribution.
+3.  Neither the name of the copyright holder nor the names of its contributors may be used
+to endorse or promote products derived from this software without specific prior
+written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************************************/
+
+#ifndef SGH3D_SOLVER_H
+#define SGH3D_SOLVER_H
+
+#include "solver.h"
+#include "state.h"
+
+// Forward declare structs
+struct SimulationParameters_t;
+struct Material_t;
+struct Mesh_t;
+struct BoundaryCondition_t;
+// struct State_t;
+struct RegionFill_t;
+struct RegionFill_host_t;
+// struct corners_in_mat_t;
+
+using namespace mtr; // matar namespace
+
+
+namespace DEC_CG_State
+{
+    // Node state to be initialized for the DEC_CG solver
+    static const std::vector<node_state> required_node_state = 
+    { 
+        node_state::coords,
+        node_state::velocity,
+        node_state::mass,
+        node_state::force,
+    };
+
+    // Gauss point state to be initialized for the DEC_CG solver
+    static const std::vector<gauss_pt_state> required_gauss_pt_state = 
+    { 
+        gauss_pt_state::volume,
+        gauss_pt_state::gradient_velocity
+    };
+
+    // Material point state to be initialized for the DEC_CG solver
+    static const std::vector<material_pt_state> required_material_pt_state = 
+    { 
+        material_pt_state::density,
+        material_pt_state::pressure,
+        material_pt_state::stress,
+        material_pt_state::specific_internal_energy,
+        material_pt_state::sound_speed,
+        material_pt_state::mass,
+        material_pt_state::volume_fraction,
+    };
+
+    // Material corner state to be initialized for the DEC_CG solver
+    static const std::vector<material_corner_state> required_material_corner_state = 
+    { 
+        material_corner_state::force
+    };
+
+    // Corner state to be initialized for the DEC_CG solver
+    static const std::vector<corner_state> required_corner_state = 
+    { 
+        corner_state::force,
+        corner_state::mass
+    };
+
+    // --- checks on fill instructions ---
+    // Node state that must be filled (setup) for the DEC_CG solver
+    static const std::vector<fill_node_state> required_fill_node_state = 
+    { 
+        fill_node_state::velocity
+    };
+
+    // Material point state that must be filled (setup) for the DEC_CG solver
+    // option A
+    static const std::vector<fill_gauss_state> required_optA_fill_material_pt_state = 
+    { 
+       fill_gauss_state::density,
+       fill_gauss_state::specific_internal_energy
+    };
+    // option B
+    static const std::vector<fill_gauss_state> required_optB_fill_material_pt_state = 
+    { 
+       fill_gauss_state::density,
+       fill_gauss_state::internal_energy
+    };
+    // option C
+    static const std::vector<fill_gauss_state> required_optC_fill_material_pt_state = 
+    { 
+       fill_gauss_state::density,
+       fill_gauss_state::stress
+    };
+    // -------------------------------------
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///
+/// \class DEC_CG
+///
+/// \brief Class for containing functions required to perform DEC_CG on 3D Cartesian meshes
+///
+/// This class contains the requisite functions requited to perform
+/// 
+///
+/////////////////////////////////////////////////////////////////////////////
+class DEC_CG : public Solver
+{
+public:
+
+    DEC_CG()  : Solver()
+    {
+    }
+
+    ~DEC_CG() = default;
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///
+    /// \fn Initialize
+    ///
+    /// \brief Initializes data associated with the DEC_CG solver
+    ///
+    /////////////////////////////////////////////////////////////////////////////
+    void initialize(SimulationParameters_t& SimulationParamaters, 
+                    Material_t& Materials, 
+                    Mesh_t& mesh, 
+                    BoundaryCondition_t& Boundary,
+                    State_t& State) const override;
+
+
+    void initialize_material_state(SimulationParameters_t& SimulationParamaters, 
+                	               Material_t& Materials, 
+                	               Mesh_t& mesh, 
+                	               BoundaryCondition_t& Boundary,
+                	               State_t& State) const override;
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///
+    /// \fn setup
+    ///
+    /// \brief Calls setup_dec_cg, which initializes state and material data
+    ///
+    /////////////////////////////////////////////////////////////////////////////
+    void setup(SimulationParameters_t& SimulationParamaters,
+        Material_t& Materials,
+        Mesh_t&     mesh,
+        BoundaryCondition_t& Boundary,
+        State_t& State) override;
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///
+    /// \fn execute
+    ///
+    /// \brief Calls the solve function which evolves the state
+    ///
+    ///
+    /////////////////////////////////////////////////////////////////////////////
+    void execute(SimulationParameters_t& SimulationParamaters,
+        Material_t& Materials,
+        BoundaryCondition_t& Boundary,
+        Mesh_t&  mesh,
+        State_t& State) override;
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///
+    /// \fn finalize
+    ///
+    /// \brief <insert brief description>
+    ///
+    /// <Insert longer more detailed description which
+    /// can span multiple lines if needed>
+    ///
+    /// \param <function parameter description>
+    /// \param <function parameter description>
+    /// \param <function parameter description>
+    ///
+    /// \return <return type and definition description if not void>
+    ///
+    /////////////////////////////////////////////////////////////////////////////
+    void finalize(SimulationParameters_t& SimulationParamaters,
+        Material_t& Materials,
+        BoundaryCondition_t& Boundary) const override
+    {
+        // Any finalize goes here, remove allocated memory, etc
+    }
+
+    
+};
+
+
+
+#endif // end HEADER_H
---
+++ single-node-refactor/src/Solvers/DEC_CG/src/dec_cg_initialize.cpp
@@ -0,0 +1,120 @@
+/**********************************************************************************************
+© 2020. Triad National Security, LLC. All rights reserved.
+This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos
+National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S.
+Department of Energy/National Nuclear Security Administration. All rights in the program are
+reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear
+Security Administration. The Government is granted for itself and others acting on its behalf a
+nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare
+derivative works, distribute copies to the public, perform publicly and display publicly, and
+to permit others to do so.
+This program is open source under the BSD-3 License.
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+1.  Redistributions of source code must retain the above copyright notice, this list of
+conditions and the following disclaimer.
+2.  Redistributions in binary form must reproduce the above copyright notice, this list of
+conditions and the following disclaimer in the documentation and/or other materials
+provided with the distribution.
+3.  Neither the name of the copyright holder nor the names of its contributors may be used
+to endorse or promote products derived from this software without specific prior
+written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************************************/
+
+#include "dec_cg_solver.h"
+#include "state.h"
+#include "mesh.h"
+#include "simulation_parameters.h"
+
+
+void DEC_CG::initialize(SimulationParameters_t& SimulationParamaters, 
+                	   Material_t& Materials, 
+                	   Mesh_t& mesh, 
+                	   BoundaryCondition_t& Boundary,
+                	   State_t& State) const
+{
+	const size_t num_nodes = mesh.num_nodes;
+    const size_t num_gauss_pts = mesh.num_elems;
+    const size_t num_corners = mesh.num_corners;
+    const size_t num_dims = mesh.num_dims;
+
+    if (num_dims != 3){
+        std::cout << "Wrong dimensions of " << num_dims << "\n";
+        throw std::runtime_error("**** Solver is for 3D coordinates, wrong dimensions specified in input ****");
+    }
+
+    // mesh state
+    State.node.initialize(num_nodes, num_dims, DEC_CG_State::required_node_state);
+    State.GaussPoints.initialize(num_gauss_pts, num_dims, DEC_CG_State::required_gauss_pt_state);
+    State.corner.initialize(num_corners, num_dims, DEC_CG_State::required_corner_state);
+
+    // check that the fills specify the required nodal fields
+    bool filled_nodal_state =
+        check_fill_node_states(DEC_CG_State::required_fill_node_state,
+                               SimulationParamaters.region_setups.fill_node_states);
+    
+    if (filled_nodal_state == false){
+        std::cout <<" Missing required nodal state in the fill instructions for the DEC_CG solver \n";
+        std::cout <<" The nodal velocity must be specified. \n" << std::endl;
+        throw std::runtime_error("**** Provide fill instructions for all required nodal variables ****");
+    }
+
+
+} // end solver initialization
+
+
+
+void DEC_CG::initialize_material_state(SimulationParameters_t& SimulationParamaters, 
+                	                  Material_t& Materials, 
+                	                  Mesh_t& mesh, 
+                	                  BoundaryCondition_t& Boundary,
+                	                  State_t& State) const
+{
+
+    // -----
+    //  Allocation of state includes a buffer, set in region_fill.cpp, which is for ALE
+    // -----
+    
+    State.MaterialToMeshMaps.initialize();
+    State.MaterialPoints.initialize(3, DEC_CG_State::required_material_pt_state); // note: dims is always 3 
+    State.MaterialCorners.initialize(3, DEC_CG_State::required_material_corner_state);
+    // zones are not used
+    
+    // NOTE: Material points are populated in the material_state_setup funcion
+
+    // check that the fills specify the required material point state fields
+    bool filled_material_state_A =
+        check_fill_mat_states(DEC_CG_State::required_optA_fill_material_pt_state,
+                              SimulationParamaters.region_setups.fill_gauss_states);
+    bool filled_material_state_B =
+        check_fill_mat_states(DEC_CG_State::required_optB_fill_material_pt_state,
+                              SimulationParamaters.region_setups.fill_gauss_states);
+    
+    // --- full stress tensor is not yet supported in region_fill ---
+    //bool filled_material_state_C =
+    //    check_fill_mat_states(SGH3D_State::required_optC_fill_material_pt_state,
+    //                                 SimulationParamaters.region_setups.fill_gauss_states);
+
+    if (filled_material_state_A == false &&
+        filled_material_state_B == false){
+        std::cout <<" Missing required material state in the fill instructions for the DEC_CG solver \n";
+        std::cout <<" The required state: \n";
+        std::cout <<"  - density \n";
+        std::cout <<"  - specific or extensive internal energy \n" << std::endl;
+        throw std::runtime_error("**** Provide fill instructions for all required material point variables ****");
+    }
+    
+    // NOTE: Material points are populated in the material_state_setup function
+
+} // end solver initialization
