ffb1516b4e2fc10842755e1cf2f7fa23856a0282
gavinwhetstone <gavinwhetstone1@gmail.com>
2026-06-12 12:37:20 -0600

    making function calls static to force it to not capture this

--- apps/multiphysics/src/Solvers/TLQS_solver_3D/include/tlqs_solver_3D.hpp
+++ apps/multiphysics/src/Solvers/TLQS_solver_3D/include/tlqs_solver_3D.hpp
@@ -242,7 +242,7 @@ public:
     // inputs: material_matrix, mesh map for nodes in the element, element id, node reference coordinates, node displacements, basis gradients wrt master element
     // outputs: displacement gradient, inverse of jacobian, jacobian determinant, 2nd PK stress in current configuration
     KOKKOS_FUNCTION
-    void get_gradients(
+    static void get_gradients(
         const double material_matrix[6][6],
         ViewCArrayKokkos <size_t>& nodes_in_elem,
         const MPICArrayKokkos <double>& coords_t0,
@@ -258,7 +258,7 @@ public:
     // inputs: material_matrix, displacement gradient, inverse Jacobian, basis gradients wrt master element, current PK2 stress
     // outputs: updated element stiffness matrix, updated element force vector
     KOKKOS_FUNCTION
-    void tally_elem_arrays(
+    static void tally_elem_arrays(
         const double material_matrix[6][6],
         const double grad_u[3][3],
         const double inv_J[3][3],
