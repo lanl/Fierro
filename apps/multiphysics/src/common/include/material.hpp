d07b398b6e8863b9ab614e81562b832853c28620
Jacob Moore <jacoblinleymoore@gmail.com>
2026-09-09 15:37:00 -0500

    ENH: Adding ALE parsing WIP

--- apps/multiphysics/src/common/include/material.hpp
+++ apps/multiphysics/src/common/include/material.hpp
@@ -51,6 +51,13 @@ namespace model
         stateBased = 2,     ///<  Model is based on the state after each stage of the time step
     };
 
+    // ALE model types
+    enum ALEType
+    {
+        noALE = 0, ///<  No ALE model used
+        ALE = 1, ///<  ALE model used
+    };
+
     // Specific strength models
     enum StrengthModels
     {
@@ -162,6 +169,12 @@ static std::map<std::string, model::StrengthType> strength_type_map
     { "no_strength", model::noStrengthType },
     { "increment_based", model::incrementBased },
     { "state_based", model::stateBased },
+};
+
+static std::map<std::string, model::StrengthType> ale_type_map
+{
+    { "no_ale", model::noALE },
+    { "ALE", model::ALE },
 };
 
 
@@ -297,6 +310,9 @@ struct MaterialEnums_t
 
     // Erosion model type: none or basic
     model::ErosionModels ErosionModels = model::noErosion;
+
+    // ALE model type: none or ALE
+    model::ALEType ALEType = model::noALE;
 
 
     // -- dissipation --
@@ -569,6 +585,7 @@ static std::vector<std::string> str_material_inps
     "normal_velocity",
     "curvature_velocity",
     "tabular_model",
+    "ale_model",
 };
 
 // ----------------------------------
