c454126e054e731572b987cdbb4ff7b6a144345a
gavinwhetstone <gavinwhetstone1@gmail.com>
2026-09-25 10:09:10 -0600

    commenting out the debug print

--- apps/multiphysics/src/Solvers/TLQS_solver_3D/src/tlqs_execute.cpp
+++ apps/multiphysics/src/Solvers/TLQS_solver_3D/src/tlqs_execute.cpp
@@ -522,7 +522,7 @@ void TLQS3D::execute(SimulationParameters_t& SimulationParamaters,
             // apply traction conditions
             boundary_stress(mesh, BoundaryConditions, rk, ref_surf, SurfQuad, State.node.coords_t0, dt, time_value, time_start, time_end);
             
-            RUN({
+            /*RUN({
                 printf("START TEST OUTPUTS\n");
                 for (int r0 = 0; r0 < rk.dims(0); r0++) {
                     for (int r1 = 0; r1 < 3; r1++) {
@@ -532,7 +532,7 @@ void TLQS3D::execute(SimulationParameters_t& SimulationParamaters,
                 }
                 printf("END TEST OUTPUTS\n");
                 Kokkos::abort("END OF TESTING RUN");
-            });
+            });*/
 
             // smoothing with chebyshev polynomial
             apply_chebyshev_preconditioner(rk, zk, D_inv, zk, delta_z, temporary, K_elem, 
