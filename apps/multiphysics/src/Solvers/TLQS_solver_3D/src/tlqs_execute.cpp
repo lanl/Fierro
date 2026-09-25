49bc95a73bd24830d85073e0464e5fe9347c3d5b
gavinwhetstone <gavinwhetstone1@gmail.com>
2026-09-25 09:33:29 -0600

    trying to debug on gpu

--- apps/multiphysics/src/Solvers/TLQS_solver_3D/src/tlqs_execute.cpp
+++ apps/multiphysics/src/Solvers/TLQS_solver_3D/src/tlqs_execute.cpp
@@ -521,15 +521,18 @@ void TLQS3D::execute(SimulationParameters_t& SimulationParamaters,
 
             // apply traction conditions
             boundary_stress(mesh, BoundaryConditions, rk, ref_surf, SurfQuad, State.node.coords_t0, dt, time_value, time_start, time_end);
-            /* std::cout << "START TEST OUTPUTS" << std::endl;
-            for (int r0 = 0; r0 < rk.dims(0); r0++) {
-                for (int r1 = 0; r1 < 3; r1++) {
-                    std::cout << rk(r0,r1) << "   ";
+            
+            RUN({
+                printf("START TEST OUTPUTS\n");
+                for (int r0 = 0; r0 < rk.dims(0); r0++) {
+                    for (int r1 = 0; r1 < 3; r1++) {
+                        printf("%f   ", rk(r0, r1));
+                    }
+                    printf("\n");
                 }
-                std::cout << std::endl;
-            }
-            std::cout << "END TEST OUTPUTS" << std::endl;
-            Kokkos::abort("END OF TESTING RUN"); */
+                printf("END TEST OUTPUTS\n");
+                Kokkos::abort("END OF TESTING RUN");
+            });
 
             // smoothing with chebyshev polynomial
             apply_chebyshev_preconditioner(rk, zk, D_inv, zk, delta_z, temporary, K_elem, 
